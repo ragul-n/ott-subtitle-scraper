35f1adaad7001d2a79a7ff536312abcebeb12fbf
Ragul Natarajan <natarajanragul8@gmail.com>
2023-01-26 11:26:32 +0530

    Adding scraper for amazon prime videos

---
+++ primevideos.py
@@ -0,0 +1,101 @@
+
+import time
+import re
+import pandas as pd
+import time
+import json
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import  Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+def init_driver():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("prefs", {
+    "profile.default_content_setting_values.notifications": 2
+    })                  
+
+    chrome_options.add_argument("--enable-logging")
+    chrome_options.add_argument("--v=1")
+    chrome_options.add_argument("user-data-dir=C:/Users/natar/AppData/Local\Google/Chrome/User Data/Profile 1") 
+
+
+    capabilities = DesiredCapabilities.CHROME
+    capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
+
+    driver = webdriver.Chrome("C:\DRIVERS\chromedriver.exe",options=chrome_options,desired_capabilities=capabilities)
+    driver.implicitly_wait(10)
+
+    return driver
+
+def is_subtitle(soup):
+    try: 
+        if 'begin' in soup.find_all("p")[0].attrs.keys():
+            return True
+    except:
+        return False
+
+def get_data(driver, url):
+    title=driver.find_element_by_xpath('//h1[@data-automation-id="title"]').text
+    year=driver.find_element_by_xpath('//span[@data-automation-id="release-year-badge"]').text
+    duration=driver.find_element_by_xpath('//span[@data-automation-id="runtime-badge"]').text
+
+    return title, year, duration
+
+def get_subtitle_file(driver):
+    
+    xhr_files = [entry for entry in driver.get_log('performance') ]# if 'fetch' in entry.get('message')
+
+    for xhr_file in xhr_files:
+        if 'request' in json.loads(xhr_file.get('message'))['message']['params'].keys():
+            url=json.loads(xhr_file.get('message'))['message']['params']['request']['url']
+            if  url.endswith('.ttml2'):
+                sub_url=json.loads(xhr_file.get('message'))['message']['params']['request']['url']
+                
+                response=requests.get(sub_url)
+
+                soup = BeautifulSoup(response.text, "html.parser")
+                if is_subtitle(soup):
+                    return response.text
+                
+
+def get_subtitle_data(driver,url):
+    driver.execute_script(f'''window.open("{url}","_blank");''')
+
+    driver.switch_to.window(driver.window_handles[-1])
+    
+    title,duration, year=get_data(driver, url)
+
+    driver.find_element_by_link_text("Continue watching").click()
+
+    sub=None
+    while not sub:
+        time.sleep(3)
+        sub=get_subtitle_file(driver)
+
+    driver.close()
+    driver.switch_to.window(driver.window_handles[-1])
+
+    return title, duration, year, sub
+
+
+
+if __name__=="__main__":
+    url='https://www.primevideo.com/region/eu/detail/0IIISZ9GJ0XQ718NNWIRPRCTE7'
+    
+    driver= init_driver()
+
+    title, duration, year,sub=get_subtitle_data(driver,url)
+
+    driver.quit()
+
+    print(title,duration,year)
+    with open(title+".xml","w",  encoding="utf-8") as f:
+        f.write(sub)
